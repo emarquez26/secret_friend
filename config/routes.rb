6a693978ed8317560953fab9d6243599cfac77b5
Edwin Marquez <emarquez26@misena.edu.co>
2014-10-08 10:38:03 -0500

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  devise_for :users
-  
+  resources :users
   get 'play/index'
   post 'play/index'
-
-  root 'users#new'
-  resources :users
 end
