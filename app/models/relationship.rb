9a09ff229163ee607f38734996fe50609e687539
Edwin Marquez <emarquez26@misena.edu.co>
2014-10-16 14:45:46 -0500

    add styles to views and index page

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,16 +3,17 @@ class Relationship < ActiveRecord::Base
     @current_user = current_user
     users_available
     if @total > 0 and @current_user.relation == false
-      player2 = @hash_user[rand(@hash_user.length)]
-      @dates_player2 = User.find(player2)
+      @player2 = @hash_user[rand(@hash_user.length)]
+      @dates_player2 = User.find(@player2)
 
-      User.change_relation @current_user.id, player2
+      User.change_relation @current_user.id, @player2
       
       save_relation = Relationship.create(
-        :player1_id => @current_user.id, :player2_id => player2,
+        :player1_id => @current_user.id, :player2_id => @player2,
         :like_player1 => @current_user.like, :like_player2 => @dates_player2.like,
         :dislike_player1 => @current_user.relation, :dislike_player2 => @dates_player2.dislike
       )
+      @dates_player2
     end
   end
 
